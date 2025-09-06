b405a8372c4c082f847fe2de639cd191a4f216ff
FrostFoe <frostfoe@gmail.com>
2025-09-06 08:01:04 +0000

    fully recheck my web app for latest changes

--- src/app/layouts/partials/Footer.jsx
+++ src/app/layouts/partials/Footer.jsx
@@ -5,8 +5,8 @@ import config from "@config/config.json";
 import menu from "@config/menu.json";
 import social from "@config/social.json";
 import { markdownify } from "@lib/utils/textConverter";
-import Link from "next/link";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Footer = () => {
   const { copyright } = config.params;
